aea15139eb807bf09d1d280e16ed22fc6fe68059
caomeichaosuannai <zzx12345>
2019-10-18 15:57:00 +0800

    111

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     这里是1914git测试
     <Login></Login>
+    <Reg></Reg>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Login from './components/Login.vue'
+import Reg from './components/reg.vue'
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    Login
+    Login,
+    Reg
   }
 }
 </script>
